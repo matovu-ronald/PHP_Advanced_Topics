488187481358c105ca86c4f80d016a618f208cd0
Matovu Ronald <maronwhite6@gmail.com>
2019-11-12 21:11:02 +0300

    Throwing SPL Exceptions

--- exceptions/exceptions.php
+++ exceptions/exceptions.php
@@ -13,13 +13,13 @@ function processCC($numb = null, $zipCode = null)
     try {
         validate($numb, $zipCode);
     } catch (Exception $e) {
-        throw new Exception('Invalid inputs', null, $e);
+        throw new BadFunctionCallException('Invalid inputs', null, $e);
     }
     echo 'processsed';
 }
 
 function validate($numb, $zipCode) {
     if (is_null($numb)) {
-        throw new Exception('No CC Number');
+        throw new InvalidArgumentException('No CC Number');
     }
 }
