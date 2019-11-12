f9bc584eeb726119e8b243f72d26d4b7bf561a08
Matovu Ronald <maronwhite6@gmail.com>
2019-11-12 21:04:45 +0300

    Nested exceptions

--- exceptions/exceptions.php
+++ exceptions/exceptions.php
@@ -4,12 +4,22 @@ try {
     processCC();
 } catch (Exception $e) {
     echo $e->getMessage();
+    echo "\n";
+    echo $e->getPrevious()->getMessage();
 }
 
 function processCC($numb = null, $zipCode = null)
 {
+    try {
+        validate($numb, $zipCode);
+    } catch (Exception $e) {
+        throw new Exception('Invalid inputs', null, $e);
+    }
+    echo 'processsed';
+}
+
+function validate($numb, $zipCode) {
     if (is_null($numb)) {
         throw new Exception('No CC Number');
     }
-    echo 'processsed';
 }
